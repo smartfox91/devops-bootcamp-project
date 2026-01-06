7dd27a74242a4bf9e1a389735ff3ae74ea697fa0
smartfox91 <mohd.adli.jaaffar@gmail.com>
2026-01-06 09:52:20 +0800

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ terraform {
     key            = "devops-bootcamp-project/terraform.tfstate"
     region         = "ap-southeast-1" #
     encrypt        = true
-    dynamodb_table = "terraform-lock-table" # Optional but recommended for state locking
+  
   }
 }
 
