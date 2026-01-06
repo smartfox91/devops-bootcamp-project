f944dd17eaf1d9dd323d3237534e0b307528b77e
smartfox91 <mohd.adli.jaaffar@gmail.com>
2026-01-06 10:17:00 +0800

    update terraform file

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
-output "ansible_inventory_file" {
-  description = "Path to generated Ansible inventory file"
-  value       = local_file.ansible_inventory.filename
-}
+# output "ansible_inventory_file" {
+#   description = "Path to generated Ansible inventory file"
+#   value       = local_file.ansible_inventory.filename
+# }
 
-output "ansible_ssh_key_file" {
-  description = "Path to generated private ssh key for Ansible"
-  value       = local_file.private_key_pem.filename
-}
+# output "ansible_ssh_key_file" {
+#   description = "Path to generated private ssh key for Ansible"
+#   value       = local_file.private_key_pem.filename
+# }
